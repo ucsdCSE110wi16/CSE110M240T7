8984ca946a097b3f698a0f5e8e102ecfe81e148c
vic023 <vic023@ucsd.edu>
2016-02-01 08:56:06 -0800

    broken code commented out
    will need to implement button in the future

--- app/src/main/java/com/example/monroe/cse110recipes/RecipeCreate.java
+++ app/src/main/java/com/example/monroe/cse110recipes/RecipeCreate.java
@@ -90,8 +90,9 @@ public class RecipeCreate extends AppCompatActivity {
         setContentView(R.layout.activity_recipe_create);
 
         mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
+        //Commented out to refer to format in the future
+        //mControlsView = findViewById(R.id.fullscreen_content_controls);
+        //mContentView = findViewById(R.id.fullscreen_content);
 
 
         // Set up the user interaction to manually show or hide the system UI.
@@ -105,7 +106,8 @@ public class RecipeCreate extends AppCompatActivity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        //Commented out to refer to format in the future
+        //findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
