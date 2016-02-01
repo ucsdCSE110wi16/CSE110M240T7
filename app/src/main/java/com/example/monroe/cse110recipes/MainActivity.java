a87f03d8f0a2d0d729ac2db34594e488a3b9e9cf
Monroe Walker <monroewlkr@yahoo.com>
2016-01-31 18:01:30 -0800

    added listview for recipes

--- app/src/main/java/com/example/monroe/cse110recipes/MainActivity.java
+++ app/src/main/java/com/example/monroe/cse110recipes/MainActivity.java
@@ -1,12 +1,24 @@
 package com.example.monroe.cse110recipes;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
-import com.parse.Parse;
-import com.parse.ParseObject;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,16 +26,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // [Optional] Power your app with Local Datastore. For more info, go to
-        // https://parse.com/docs/android/guide#local-datastore
-        Parse.enableLocalDatastore(this);
 
-        Parse.initialize(this);
-        ParseObject testObject = new ParseObject("TestObject");
+        final ListView listview = (ListView) findViewById(R.id.listview);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+                "Android", "iPhone", "WindowsMobile" };
 
+        final ArrayList<String> list = new ArrayList<String>();
+        final ArrayList<Recipe> list2 = new ArrayList<Recipe>();
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+            list2.add(new Recipe(values[i], 30, 2));
+        }
+        final StableArrayAdapter adapter = new StableArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+        final StableArrayAdapter2 adapter2 = new StableArrayAdapter2(this,
+                R.layout.recipe_listview, list2);
+        listview.setAdapter(adapter2);
 
-        testObject.put("foo", "bar");
-        testObject.saveInBackground();
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                final Recipe item = (Recipe) parent.getItemAtPosition(position);
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setTitle("Alert");
+                alertDialog.setMessage("id: "+item.id);
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+//                view.animate().setDuration(2000).alpha(0)
+//                        .withEndAction(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                list.remove(item);
+//                                adapter.notifyDataSetChanged();
+//                                view.setAlpha(1);
+//                            }
+//                        });
+            }
+
+        });
     }
 
     @Override
@@ -46,5 +96,71 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class StableArrayAdapter2 extends ArrayAdapter<Recipe> {
+
+        HashMap<Recipe, Integer> mIdMap = new HashMap<Recipe, Integer>();
+        List<Recipe> recipes;
+        Context context;
+
+        public StableArrayAdapter2(Context context, int textViewResourceId,
+                                  List<Recipe> objects) {
+            super(context, textViewResourceId, objects);
+            recipes = objects;
+            this.context = context;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View rowView = inflater.inflate(R.layout.recipe_listview, parent, false);
+            TextView textView = (TextView) rowView.findViewById(R.id.recipeName);
+            TextView timeView = (TextView) rowView.findViewById(R.id.recipeTime);
+            textView.setText(recipes.get(position).name);
+            // change the icon for Windows and iPhone
+            String time = recipes.get(position).minutes + "minutes";
+            ((RatingBar) rowView.findViewById(R.id.recipeRating)).setRating((float)recipes.get(position).rating);
+
+            return rowView;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            Recipe item = getItem(position);
+            return item.id;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+    }
+
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
     }
 }
