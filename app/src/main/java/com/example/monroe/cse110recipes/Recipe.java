028004e4fde3a32fc43a02c3f298957410dbbb06
clifftai <cctai@ucsd.edu>
2016-02-01 20:18:30 -0800

    Recipe.java updates and save/load to memory capabilities
    updated recipe.java, and will continue to add more functionality through
    the week.
    commented some lines out of RecipeCreate.java to allow it to compile.

--- app/src/main/java/com/example/monroe/cse110recipes/Recipe.java
+++ app/src/main/java/com/example/monroe/cse110recipes/Recipe.java
@@ -1,18 +1,67 @@
 package com.example.monroe.cse110recipes;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import android.content.Context;
 /**
  * Created by monroe on 1/31/2016.
+ * edited by Cliff on 2/1/2016
  */
 public class Recipe {
-    String name;
-    int minutes;
-    int rating;
-    int id;
+    String name; //recipe name
+    int minutes; //time to cook
+    int rating; //rating of the recipe
+    int id; //id of the recipe??
     private static int _id = 0;
+
+    //basic constructor
+    public Recipe () {
+
+    }
+    //constructor with some parameters
     public Recipe(String name, int minutes, int rating){
         this.name = name;
         this.minutes = minutes;
         this.rating = rating;
         this.id = _id++;
+    }
+    
+    String[] tags; //the series of tags that can be used to search for the recipe
+    int difficulty; //difficulty level
+    String lastUsed; //date the recipe was last accessed
+    String[] steps; //list of the steps in string format, ordered chronologically
+    int[] timePerStep; //time that each step takes, indexed the same as steps[]
+    
+    //Serialize and save a file on an android device
+    public void saveRecipe(Context context) {
+    	try {
+            FileOutputStream fos = context.openFileOutput(name, Context.MODE_PRIVATE);
+            ObjectOutputStream os = new ObjectOutputStream(fos);
+            os.writeObject(this);
+            os.close();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    //retrieve a serialized file from an android device
+    public Recipe loadRecipe(Context context)  {
+        Recipe recipe = null;
+        try {
+            FileInputStream fis = context.openFileInput(this.name);
+            ObjectInputStream is = new ObjectInputStream(fis);
+            recipe = (Recipe) is.readObject();
+            is.close();
+            fis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return recipe;
     }
 }
