71a80950da7d28e321bfa6a928e79f3ece81a397
Bao Liang <b2liang@ucsd.edu>
2016-02-11 15:44:18 -0800

    Update Test

--- app/src/androidTest/java/com/example/monroe/cse110recipes/ApplicationTest.java
+++ app/src/androidTest/java/com/example/monroe/cse110recipes/ApplicationTest.java
@@ -1,20 +1,38 @@
 package com.example.monroe.cse110recipes;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
-import android.app.Fragment;
-import android.app.Instrumentation;
-import android.test.ActivityInstrumentationTestCase2;
-import android.test.TouchUtils;
-import android.test.ViewAsserts;
-import android.view.View;
 import android.app.Instrumentation.ActivityMonitor;
-import android.widget.Button;
-import android.test.InstrumentationTestCase;
-import android.test.ActivityTestCase;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
-public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivity> {
-    public ApplicationTest() {
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static junit.framework.Assert.assertNotNull;
+
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
+
+    @Test
+    public void testSearchByRecipeClick(){
+
+        ActivityMonitor activityMonitor = getInstrumentation().addMonitor(RecipeListActivity.class.getName(), null, false);
+        onView(withId(R.id.button3)).perform(click());
+        RecipeListActivity nextActivity = (RecipeListActivity) getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 5000);
+        // next activity is opened and captured.
+        assertNotNull(nextActivity);
+        nextActivity.finish();
+    }
+
+
+   /* public ApplicationTest() {
         super(MainActivity.class);
     }
 
@@ -39,5 +57,5 @@ public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivi
         // next activity is opened and captured.
         assertNotNull(nextActivity);
         nextActivity .finish();
-    }
+    }*/
 }
