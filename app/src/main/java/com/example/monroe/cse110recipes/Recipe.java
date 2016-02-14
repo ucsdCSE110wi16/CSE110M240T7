1ed8241d6cb583f46298355af4897e0e3ea486ff
clifftai <cctai@ucsd.edu>
2016-02-13 21:02:31 -0800

    Some DataBase Changes
    Did some (unnecessary?) SQLite changes, but all I think we need is file
    saving and reading

--- app/src/main/java/com/example/monroe/cse110recipes/Recipe.java
+++ app/src/main/java/com/example/monroe/cse110recipes/Recipe.java
@@ -6,17 +6,37 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 /**
  * Created by monroe on 1/31/2016.
  * edited by Cliff on 2/1/2016
  */
-public class Recipe {
+public class Recipe {//extends SQLiteOpenHelper{
+    private static final String DATABASE_NAME = "Recipe_Database";
+    private static final int DATABASE_VERSION = 1;
+    public static final String COLUMN_ID = "_id";
+    public static String strSeparator = "_,_";
+
+
     String name; //recipe name
     int minutes; //time to cook
     int rating; //rating of the recipe
     int id; //id of the recipe??
     boolean favorite = false;
     private static int _id = 0;
+/*
+    public final String DATABASE_CREATE_OBJECT = "create table " +
+            this.name + "(" + COLUMN_ID + "integer primary key autoincrement, "
+            + this.minutes + "int, "
+            + this.rating + "int, "
+            + this.id + "int, "
+            + this.favorite + "int, "
+            + this.tags + "string, "
+            + this.difficulty + "int, "
+            + this.lastUsed + "string, "
+            + this.steps + "string, "
+            + this.timePerStep + "int);"; */
 
     //setter methods
     public void setName(String name){
@@ -52,16 +72,28 @@ public class Recipe {
     }
 
     //basic constructor
+
     public Recipe () {
 
     }
-    //constructor with some parameters
-    public Recipe(String name, int minutes, int rating){
+    public Recipe (String name, int minutes, int rating) {
         this.name = name;
         this.minutes = minutes;
         this.rating = rating;
         this.id = _id++;
     }
+    /*
+    public Recipe (Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    //constructor with some parameters
+    public Recipe(String name, int minutes, int rating, Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.name = name;
+        this.minutes = minutes;
+        this.rating = rating;
+        this.id = _id++;
+    }*/
     
     String[] tags; //the series of tags that can be used to search for the recipe
     int difficulty; //difficulty level
@@ -97,5 +129,29 @@ public class Recipe {
             e.printStackTrace();
         }
         return recipe;
+    } /*
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL(DATABASE_CREATE_OBJECT);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+            db.execSQL("Drop Table If Exists" + this.name);
+            onCreate(db);
+    } */
+    public static String convertArrayToSTring(String[] array) {
+        String str = "";
+        for (int i = 0; i < array.length; i++) {
+            str = str+array[i];
+            if (i<array.length - 1) {
+                str = str +strSeparator;
+            }
+        }
+        return str;
+    }
+    public static String[] convertStringToArray (String str) {
+        String[] arr = str.split(strSeparator);
+        return arr;
     }
 }
