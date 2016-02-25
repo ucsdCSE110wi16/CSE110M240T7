c9ae975c8b3ce3a0610825b9c7feccb292cd4a1c
Bao Liang <b2liang@ucsd.edu>
2016-02-24 20:01:18 -0800

    bao

--- app/src/androidTest/java/com/example/monroe/cse110recipes/ApplicationTest.java
+++ app/src/androidTest/java/com/example/monroe/cse110recipes/ApplicationTest.java
@@ -12,7 +12,7 @@ import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static junit.framework.Assert.assertNull;
+import static junit.framework.Assert.assertNotNull;
 
 @RunWith(AndroidJUnit4.class)
 public class ApplicationTest {
@@ -31,7 +31,7 @@ public class ApplicationTest {
         nextActivity.finish();
     }
 
-    @Test
+
     /*public void testSearchByIngredientsClick(){
 
         ActivityMonitor activityMonitor = getInstrumentation().addMonitor(RecipeListActivity.class.getName(), null, false);
