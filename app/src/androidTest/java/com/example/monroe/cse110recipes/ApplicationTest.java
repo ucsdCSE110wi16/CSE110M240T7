5ab1e504e60aea55a34dd2926f0aa7e50e83ada7
Bao Liang <b2liang@ucsd.edu>
2016-02-24 19:56:25 -0800

    Bao

--- app/src/androidTest/java/com/example/monroe/cse110recipes/ApplicationTest.java
+++ app/src/androidTest/java/com/example/monroe/cse110recipes/ApplicationTest.java
@@ -12,7 +12,7 @@ import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertNull;
 
 @RunWith(AndroidJUnit4.class)
 public class ApplicationTest {
@@ -31,31 +31,16 @@ public class ApplicationTest {
         nextActivity.finish();
     }
 
+    @Test
+    /*public void testSearchByIngredientsClick(){
 
-   /* public ApplicationTest() {
-        super(MainActivity.class);
-    }
-
-    public void testOpenNextActivity() {
-        // register next activity that need to be monitored.
         ActivityMonitor activityMonitor = getInstrumentation().addMonitor(RecipeListActivity.class.getName(), null, false);
-
-        // open current activity.
-        MainActivity myActivity = getActivity();
-        final Button button = (Button) myActivity.findViewById(R.id.button2);
-        myActivity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                // click button and open next activity.
-                button.performClick();
-            }
-        });
-
-        //Watch for the timeout
-        //example values 5000 if in ms, or 5 if it's in seconds.
-        RecipeListActivity nextActivity = (RecipeListActivity)getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 5000);
+        onView(withId(R.id.button3)).perform(click());
+        RecipeListActivity nextActivity = (RecipeListActivity) getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 5000);
         // next activity is opened and captured.
         assertNotNull(nextActivity);
-        nextActivity .finish();
+        nextActivity.finish();
     }*/
+
+
 }
