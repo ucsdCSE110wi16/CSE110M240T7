e7f8e8a488591bef20a8cac037563a35367ec0a3
Bao Liang <b2liang@ucsd.edu>
2016-03-07 17:18:43 -0800

    try to do but do nothing

--- app/src/androidTest/java/com/example/monroe/cse110recipes/ApplicationTest.java
+++ app/src/androidTest/java/com/example/monroe/cse110recipes/ApplicationTest.java
@@ -1,21 +1,9 @@
 package com.example.monroe.cse110recipes;
 
-import android.app.Activity;
 import android.app.Instrumentation.ActivityMonitor;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.Espresso;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.view.View;
-import android.widget.Adapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
-
-import com.example.monroe.cse110recipes.recipes.Recipe;
 import com.example.monroe.cse110recipes.recipes.RecipeCreateActivity;
 import com.example.monroe.cse110recipes.recipes.RecipeListActivity;
 
@@ -23,27 +11,12 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.InstrumentationRegistry.getContext;
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
-import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.core.deps.guava.base.Preconditions.checkNotNull;
-import static android.support.test.espresso.matcher.PreferenceMatchers.withTitle;
-import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static com.example.monroe.cse110recipes.R.id.action_favorited;
-import static com.example.monroe.cse110recipes.R.id.recipe;
-import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.fail;
-import static org.hamcrest.EasyMock2Matchers.equalTo;
-import static org.hamcrest.Matchers.anything;
-import static org.hamcrest.Matchers.not;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -53,18 +26,21 @@ public class ApplicationTest {
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
 
     @Test
-    public void test1(){
+    public void Test1(){
 
         ActivityMonitor activityMonitor = getInstrumentation().addMonitor(RecipeListActivity.class.getName(), null, false);
         onView(withId(R.id.button3)).perform(click());
         RecipeListActivity nextActivity = (RecipeListActivity) getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 5000);
         // next activity is opened and captured.
         assertNotNull(nextActivity);
+
         nextActivity.finish();
     }
 
+
+
     @Test
-    public void test2() {
+    public void Test2() {
 
         ActivityMonitor activityMonitor = getInstrumentation().addMonitor(RecipeCreateActivity.class.getName(), null, false);
         onView(withId(R.id.button3)).perform(click());
@@ -76,8 +52,8 @@ public class ApplicationTest {
 
 
 
-    @Test
-    public void test3() {
+  /*  @Test
+    public void Test3() {
         ActivityMonitor activityMonitor = getInstrumentation().addMonitor(RecipeCreateActivity.class.getName(), null, false);
         onView(withId(R.id.button3)).perform(click());
         onView(withId(R.id.action_add)).perform(click());
@@ -85,19 +61,23 @@ public class ApplicationTest {
         onView(withId(R.id.edit_time)).perform(click(), typeText("10"));
         onView(withId(R.id.edit_rating)).perform(click());
         onView(withId(R.id.action_save)).perform(click());
-        onData(anything()).inAdapterView(withId(R.id.listview)).atPosition(0).perform(click());
-        onView(withId(R.id.recipe)).check(matches(withText("TestSaveButton")));
-        onView(withId(R.id.cook_Time)).check(matches(withText("10")));
+        RecipeListActivity r = new RecipeListActivity();
+        (r.recipes.get(0).getName());
+
+
+        //onData(anything()).inAdapterView(withId(R.id.listview)).atPosition(0).perform(click());
+
+       // onView(withId(R.id.recipe)).check(matches(withText("TestSaveButton")));
+        //onView(withId(R.id.cook_Time)).check(matches(withText("10")));
 
     }
 
 
-    private String getString(int resId){
-        return getInstrumentation().getTargetContext().getString(resId);
-    }
+
 /*
     @Test
-    public void test4(){
+    public void Test4(){
+
         onView(withId(R.id.button3)).perform(click());
         onView(withId(R.id.action_add)).perform(click());
         onView(withId(R.id.edit_title)).perform(click(), typeText("TestLikeButton"));
@@ -107,14 +87,13 @@ public class ApplicationTest {
 
         onData(anything()).inAdapterView(withId(R.id.listview)).atPosition(0).perform(click());
         //onView(withContentDescription(getString(R.id.action_favorited))).perform(click());
-       openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
-        onView(withText("Favorite")).perform(click());
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withId(R.id.action_favorited)).perform(click());
        // onData(anything()).inAdapterView(withId(R.id.action_notfavorited)).perform(click());
         //onView(withId(R.id.home)).perform(click());
         Espresso.pressBack();
         Espresso.pressBack();
         onView(withId(R.id.button5)).perform(click());
-
     }*/
    /* public static Matcher<Object> withItemContent(String expectedText) {
         checkNotNull(expectedText);
@@ -122,8 +101,24 @@ public class ApplicationTest {
     }
 
 
+
     public static Matcher<View> withAdaptedData(final Matcher<Object> dataMatcher) {
         return new TypeSafeMatcher<View>() {
+
+     @Test
+    public void TestFilter(){
+         onView(withId(R.id.button3)).perform(click());
+         onView(withId(R.id.edit_title)).perform(click(), typeText("H"));
+         onView(withId(R.id.filter_button)).perform(click());
+
+         DataInteraction c = onData(Matchers.anything()).inAdapterView(withId(R.id.listview)).atPosition(0);
+         
+     }
+
+
+
+      onData(hasToString)
+
 
     @Override
     public void describeTo(Description description) {
@@ -149,8 +144,8 @@ public class ApplicationTest {
 }*/
 
     //@SuppressWarnings("unchecked")
- /*   @Test
-    public void test5(){
+   /* @Test
+    public void test5() {
         onView(withId(R.id.button3)).perform(click());
         onView(withId(R.id.action_add)).perform(click());
         onView(withId(R.id.edit_title)).perform(click(), typeText("TestFilter_Apple"));
@@ -158,7 +153,7 @@ public class ApplicationTest {
         onView(withId(R.id.edit_rating)).perform(click());
         onView(withId(R.id.action_save)).perform(click());
 
-       /* onView(withId(R.id.action_add)).perform(click());
+        onView(withId(R.id.action_add)).perform(click());
         onView(withId(R.id.edit_title)).perform(click(), typeText("TestFilter_Banana"));
         onView(withId(R.id.edit_time)).perform(click(), typeText("5"));
         onView(withId(R.id.edit_rating)).perform(click());
@@ -167,13 +162,15 @@ public class ApplicationTest {
         onView(withId(R.id.filter_recipe_name)).perform(click(), typeText("Apple"));
         onView(withId(R.id.filter_button)).perform(click());
         onData(anything()).inAdapterView(withId(R.id.listview)).atPosition(0).perform(click());
-        onView(withId(R.id.recipe)).check(matches(withText("TestFilter_Apple")));
+        onView(allOf(withId(R.id.recipe), withText("TestFilter_Apple"))).check(matches(isDisplayed()));
+
+        //onView(withId(R.id.recipe)).check(matches(withText("TestFilter_Apple")));
         //onView(withId(R.id.recipe)).check(matches(withText("TestFilter_Apple")));
         //onView(withId(R.id.cook_Time)).check(matches(withText("5")));
 
         /*onView(withId(R.id.listview))
                 .check(matches(not(withAdaptedData(withItemContent("hello")))));*/
- // }
+   // }
 
   /*  public void testFilter() {
         onView(withId(R.id.button3)).perform(click());
